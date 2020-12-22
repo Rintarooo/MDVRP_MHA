a267b72efaa1c8441ed9490a8f914b1338cba33b
Rintarooo <310rnomeado@gmail.com>
2020-12-22 23:43:06 +0900

    update

--- dataclass.py
+++ dataclass.py
@@ -138,14 +138,14 @@ def get_dist_mat(data, scale_up = 1e4):# digit = 2
 
 if __name__ == '__main__':
 	device = torch.device('cpu')# torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')	
-	n_depot = 2
-	n_car = 12
+	n_depot = 3
+	n_car_each_depot = 4#12
 	n_customer = 50
 	seed = 123
 	capa = 2.
 
 	from Torch.dataset import generate_data
-	data = generate_data(device, batch = 1, n_car = n_car, n_depot = n_depot, n_customer = n_customer, capa = capa, seed = seed)
+	data = generate_data(device, batch = 1, n_car_each_depot = n_car_each_depot, n_depot = n_depot, n_customer = n_customer, capa = capa, seed = seed)
 	
 	basename = f'd{n_depot}n{n_customer}D{int(capa)}.json'
 	dirname1 = 'Torch/data/'
