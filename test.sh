058e7b22a5e65570766699ad58bbbaea8a9c7012
Rintarooo <310rnomeado@gmail.com>
2021-01-15 21:42:01 +0900

    update

--- test.sh
+++ test.sh
@@ -4,11 +4,13 @@ echo "run $0"
 n_depot=3
 n_car_each_depot=5
 n_customer=20
-seed=0
+#seed=0
 capa=1.
 
 # echo "n_depot: ${n_depot}"
-python dataclass.py ${n_depot} ${n_car_each_depot} \
-	${n_customer} ${seed} ${capa}
-
+for seed in $(seq 1 10)
+do
+	python dataclass.py ${n_depot} ${n_car_each_depot} \
+		${n_customer} ${seed} ${capa}
+done
  
