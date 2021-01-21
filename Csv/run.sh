0a2306c2623ac1745500051f29a6ea548d46a4e7
Rintarooo <310rnomeado@gmail.com>
2021-01-21 22:50:04 +0900

    update

--- Csv/run.sh
+++ Csv/run.sh
@@ -6,3 +6,4 @@ find . -iname "n50d3c3D2*.csv" | xargs tail -n 2
 find . -iname "n50d3c5D1*.csv" | xargs tail -n 2
 find . -iname "n50d1c6D2*.csv" | xargs tail -n 2
 find . -iname "n100d3c2D3*.csv" | xargs tail -n 2
+find . -iname "n100d1c6D3*.csv" | xargs tail -n 2
--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "running $0 ..."
-n_depot=3
-n_car_each_depot=2
+n_depot=1
+n_car_each_depot=6
 n_customer=100
 #seed=0
 #capa is float value
