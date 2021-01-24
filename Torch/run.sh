8b738ab551b22207367582159e88986a2d85ffd2
Rintarooo <310rnomeado@gmail.com>
2021-01-24 12:05:07 +0900

    update

--- Torch/run.sh
+++ Torch/run.sh
@@ -3,4 +3,5 @@
 # python plot.py -p Weights/VRP20_epoch19.pt -t data/n20d3c5D1s2.json -b 512
 # python plot.py -p Weights/VRP50_epoch18.pt -t data/n50d3c5D1s3.json -b 512
 # python plot.py -p Weights/VRP100_epoch15.pt -t data/n100d3c2D3s2.json -b 512
-python plot.py -p Weights/VRP50_epoch18.pt -t data/n50d3c3D2s2.json -b 512
+# python plot.py -p Weights/VRP50_epoch18.pt -t data/n50d3c3D2s2.json -b 512
+python plot.py -p Weights/VRP100_epoch15.pt -t data/n120d3c3D3s2.json -b 512
