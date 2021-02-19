1972e6c72178565413d1b121d6427e0680974d4e
Rintarooo <310rnomeado@gmail.com>
2021-02-19 14:50:13 +0900

    add docker

---
+++ .bash_history
@@ -0,0 +1,9 @@
+ls
+vim samp.txt
+ls
+ls
+python
+vim sam.txt
+sudo python
+sudo
+ls
---
+++ docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# default:test:env
+IMAGE_NAME=${2:-test:env}
+
+if [ "$1" = "build" ]; then
+	docker build -t $IMAGE_NAME .
+	echo -e "\n\n\ndocker images | head"
+	docker images | head
+elif [ "$1" = "run" ]; then
+	docker run -it --rm  \
+		--gpus=all \
+		-v ${PWD}:/home/user \
+		$IMAGE_NAME
+		#test:env
+else
+	echo "command should be:
+	 ${0} build
+	 ${0} run"
+fi
